7fd4a588b6bb92d7898be1826937aee0835fd86e
Yasmin-Abou-Hamra <Yasmin.AbouHamra@Student.HTW-Berlin.de>
2025-10-22 10:01:56 +0200

    Update

--- src/main/java/com/example/demo/WishEntry.java
+++ src/main/java/com/example/demo/WishEntry.java
@@ -1,7 +1,52 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class WishEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String title;
+    private Long id;
+    private String name;
     private String description;
+    private String status;
     private int price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
--- src/main/java/com/example/demo/WishEntryController.java
+++ src/main/java/com/example/demo/WishEntryController.java
@@ -2,13 +2,28 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class WishEntryController {
 
-    @GetMapping("/wishes")
+    @Autowired
+    private WishEntryRepository wishEntryRepository;
+
+    @GetMapping("/hello")
     public String getWishes() {
         return "Hello World";
+    }
+    @GetMapping("/wishes")
+    public List<WishEntry> getAllWishes() {
+        return wishEntryRepository.findAll();
+    }
+
+    @PostMapping("/wishes")
+    public WishEntry addWish(@RequestBody WishEntry wishEntry) {
+        return wishEntryRepository.save(wishEntry);
     }
 }
 
