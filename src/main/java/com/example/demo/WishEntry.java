d543b7678f8ec5f0795dba9123d78c9be9cf0781
Yasmin-Abou-Hamra <Yasmin.AbouHamra@Student.HTW-Berlin.de>
2025-10-22 23:44:13 +0200

    Updated backend: modified WishEntry and WishEntryController

--- src/main/java/com/example/demo/WishEntry.java
+++ src/main/java/com/example/demo/WishEntry.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class WishEntry {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String title;
--- src/main/java/com/example/demo/WishEntryController.java
+++ src/main/java/com/example/demo/WishEntryController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8081")
 public class WishEntryController {
 
     @Autowired
