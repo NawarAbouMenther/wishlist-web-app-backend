6002f589513c400e6b227fa227098406879af39e
Yasmin-Abou-Hamra <Yasmin.AbouHamra@Student.HTW-Berlin.de>
2025-10-25 23:10:04 +0200

    Update M2

--- src/main/java/com/example/demo/WishEntry.java
+++ src/main/java/com/example/demo/WishEntry.java
@@ -1,17 +1,17 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class WishEntry {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String title;
     private Long id;
+    private String title;
     private String name;
     private String description;
     private String status;
