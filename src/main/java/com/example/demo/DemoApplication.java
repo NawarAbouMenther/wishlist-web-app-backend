7c14f453232de9b282999ad524c0918bee6b892a
NawarAbouMenther <nawarabomonter@gmail.com>
2026-01-13 18:37:13 +0100

    Kategorien gespeichert

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,41 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initCategories(WishCategoryRepository repo) {
+		return args -> {
+			if (repo.count() == 0) {
+				WishCategory c1 = new WishCategory();
+				c1.setKey("birthday");
+				c1.setLabel("Geburtstag");
+
+				WishCategory c2 = new WishCategory();
+				c2.setKey("christmas");
+				c2.setLabel("Weihnachten");
+
+				WishCategory c3 = new WishCategory();
+				c3.setKey("wedding");
+				c3.setLabel("Hochzeit");
+
+				WishCategory c4 = new WishCategory();
+				c4.setKey("baby");
+				c4.setLabel("Baby-Party");
+
+				WishCategory c5 = new WishCategory();
+				c5.setKey("wishlist");
+				c5.setLabel("Wunschliste");
+
+				WishCategory c6 = new WishCategory();
+				c6.setKey("custom");
+				c6.setLabel("Eigene Kategorie");
+
+				repo.save(c1);
+				repo.save(c2);
+				repo.save(c3);
+				repo.save(c4);
+				repo.save(c5);
+				repo.save(c6);
+			}
+		};
+	}
 }
